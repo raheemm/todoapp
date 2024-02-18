1c2aa2f59b5ef24dad6fd2df41b384e19e25ed3d
Raheem Sarcar <raheem@rewardcamp.com>
2024-02-18 09:40:53 -0500

    new functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,60 @@ class TasksController < ApplicationController
     def index
         @tasks = Task.all 
     end 
+
+    def show
+        @tasks = Task.all
+    end 
+
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        redirect_to @task
+    end 
+
+    def done
+        @task = Task.find(params[:id])
+        @task.update(status: 'true')
+        redirect_to @task
+    end 
+
+    def undo
+        @task = Task.find(params[:id])
+        @task.update(status: 'false')
+        redirect_to @task
+    end 
+
+    def new
+        @task = Task.new
+    end 
+
+    def edit
+        @task = Task.find(params[:id])
+    end 
+
+    def create
+        task_params = params.require(:task).permit(:title, :status)
+        @task = Task.new(task_params)
+        if @task.save
+            flash[:notice] = "Task was created"
+            redirect_to @task
+        else 
+            redirect_to 'new'
+        end 
+    end 
+
+    def update
+        @task = Task.find(params[:id])
+        task_params = params.require(:task).permit(:title, :status)
+        task_params[:status] = task_params[:status] == '1'  
+
+        if @task.update(task_params)
+            flash[:notice] = "Tasks updated successfully"
+            redirect_to @task
+        else 
+            render 'edit'
+        end 
+    end 
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'tasks#index'
+  resources :tasks
+
+  post '/done/:id', to: 'tasks#done', as: 'done'
+  get '/done/:id', to: 'tasks#index'
+
+  post '/undo/:id', to: 'tasks#undo', as: 'undo'
+  get '/undo/:id', to: 'tasks#index'
 
 end
