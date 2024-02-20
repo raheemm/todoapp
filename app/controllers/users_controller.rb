d24f23b1fc81c67de7005689ce0999ae42305208
Raheem Sarcar <raheem@rewardcamp.com>
2024-02-19 22:26:38 -0500

    Create all users listing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,29 @@ class UsersController < ApplicationController
         @user = User.new 
     end 
 
+    def index
+        @users = User.all 
+    end 
+
+    def show
+        @user = User.find(params[:id])
+        @tasks = @user.tasks
+    end 
+
+    def edit
+        @user = User.find(params[:id])
+    end 
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "Your profile was updated"
+            redirect_to tasks_path
+        else 
+            render 'edit'
+        end 
+    end 
+
     def create
         @user = User.new(user_params)
         if @user.save 
